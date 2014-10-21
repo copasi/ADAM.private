23982905397454b9074dfb58512f77252272d702
David Murrugarra <murrugarra@uky.edu>
2014-10-21 09:14:45 -0400

    SDDScpp was renamed as SDDS_control

---
+++ SDDS_control/run.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+require './algorun/Model.rb'
+input = File.read(ARGV[0])
+json = JSON.parse(input)
+model=Model.new(json,'./SDDS')
+model.run()
+puts model.render_output()
+model.clean_temp_files()
