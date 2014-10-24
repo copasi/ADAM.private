4215f724685cb22277e3bb4293e50a32636e2fa2
Seda Arat <seda8arat@gmail.com>
2014-10-24 13:03:30 -0400

    "Documentation and changes in the RandomSampling module were finished and ready to be tested in the pipeline."

--- superADAM/RandomSampling/src/RandomSampling.pl
+++ superADAM/RandomSampling/src/RandomSampling.pl
@@ -1,6 +1,6 @@
 # Author: Seda Arat
 # Name: Basin of Attractors for Large Systems using Random Sampling Synch Update
-# Revision Date: October 2014
+# Revision Date: October 24, 2014
 
 #!/usr/bin/perl
 
@@ -18,17 +18,23 @@ use JSON;
 
 ############################################################
 
+ # TO_DO: use the transition table information if available
+
+ # TO_DO: use hash/array references
+
+############################################################
+
 =head1 NAME
 
 RandomSampling.pl - Find the basin of attractors of a given discrete system with a large state space using random sampling and a synchronous update schedule.
 
 =head1 USAGE
 
-RandomSampling.pl -m <model-file> -s <sampling-file> -o <output-file>
+RandomSampling.pl -i <input-file> -o <output-file>
 
 =head1 SYNOPSIS
 
-RandomSampling.pl -m <model-file> -s <sampling-file> -o <output-fiel>
+RandomSampling.pl -i <input-file> -o <output-file>
 
 =head1 DESCRIPTION
 
@@ -38,17 +44,9 @@ RandomSampling.pl - Find the basin of attractors of a given discrete system with
 
 =over
 
-=item -m[odel-file] <model-file>
+=item -i[nput-file] <input-file>
 
-The JSON file containing the model information (.json). 
-
-=for Euclid:
-
-network-file.type: readable
-
-=item -s[ampling-file] <sampling-file>
-
-The JSON file containing the sampling information that the user has been specified (.json). 
+The JSON file containing the input information (.json). 
 
 =for Euclid:
 
@@ -56,7 +54,7 @@ network-file.type: readable
 
 =item -o[utput-file] <output-file>
 
-The JSON file containing the basin of attractions of the system.
+The JSON file containing the average trajectories of variables of interest.
 
 =for Euclid:
 
@@ -73,91 +71,39 @@ Seda Arat
 # it is for random number generator
 srand ();
 
-# inputs
-my $modelFile = $ARGV{'-m'};
-my $samplingFile = $ARGV{'-s'};
+# input
+my $inputFile = $ARGV{'-i'};
 
-# output(s)
+# output
 my $outputFile = $ARGV{'-o'};
 
-# converts Model.json to Perl format
-my $model = JSON::Parse::json_file_to_perl ($modelFile);
+# converts input.json to Perl format
+my $task = JSON::Parse::json_file_to_perl ($inputFile);
+my $input = $task->{'task'}->{'input'}->[0];
+my $samplingSize = $task->{'task'}->{'method'}->{'arguments'}->[0]->{'samplingSize'};
 
-# converts Simulation.json to Perl format
-my $sampling = JSON::Parse::json_file_to_perl ($samplingFile);
-
-# sets the update rules/functions (hash)
-my $updateFunctions = $model->{'model'}->{'updateRules'};
-my $num_functions = scalar values %$updateFunctions;
+# sets the update rules/functions (array)
+my $updateRules = $input->{'updateRules'};
+my $num_rules = scalar @$updateRules;
 
 # sets the number of variables in the model (array)
-my $variables = $model->{'model'}->{'variables'};
+my $variables = $input->{'variables'};
 my $num_variables = scalar @$variables;
 
 # sets the unified (maximum prime) number that each state can take values up to (scalar)
-my $num_states = $sampling->{'sampling'}->{'numberofStates'};
+my $num_states = $input->{'fieldCardinality'};
 
-# sets the sampling size
-my $samplingSize = $sampling->{'sampling'}->{'samplingSize'};
+# upper limits
+my $max_num_sampling = 10**6;
 
-error_checking ();
+errorCheck ();
+my ($polyFuncs, $tranTables) = get_updateRules ();
+my ($statesAndAttractors, $basinOfAttractors) = get_basinOfAttractors ();
+my $output = format_output ();
 
-my $stateSpaceSize = $num_states ** $num_functions;
-my $length = 5;
-my %state_attractor_table;
-my %attractor_table;
-
-for (my $index = 1; $index <= $samplingSize; $index++) {
-  my $is = 1 + int (rand ($stateSpaceSize + 1));
-
-  if (exists $state_attractor_table{$is}) {
-    next;
-  }
-
-  my @array = ($is);
-  
-  attr: while (1) {
-    for (my $n = 1; $n <= $length; $n++) {
-      push (@array, get_nextstate ($array[-1]));
-    }
-
-    my $arraysize = scalar @array;
-    
-    for (my $j = 0; $j < $arraysize - 1; $j++) {
-      for (my $k = $j + 1; $k < $arraysize; $k++) {
-	if ($array[$j] == $array[$k]) {
-
-	  my @sub_array = @array[$j ... $k - 1];
-	  my $sortedAttractor = join (' , ', sort {$a <=> $b} @sub_array);
-
-	  unless (exists $attractor_table{$sortedAttractor} ) {
-	    my $attractor = join (' -> ', @sub_array);
-	    $attractor_table{$sortedAttractor} = [$attractor, 0];
-	  }
-
-	  for (my $s = 0; $s < $k; $s++) {
-	    my $a = $array[$s];
-	    unless (exists $state_attractor_table{$a}) {
-	      $state_attractor_table{$a} = $sortedAttractor;
-	      ${$attractor_table{$sortedAttractor}}[1]++;
-	    }
-	  }
-	  last attr;
-	}
-      }
-    }
-
-    push (@array, get_nextstate ($array[-1]));
-
-  } # end of while loop
-
-} # end of for loop
-
-my $attractorTable = format_attractors (\%attractor_table);
 my $json = JSON->new->indent ();
-
 open (OUT," > $outputFile") or die ("<br>ERROR: Cannot open the file for output. <br>");
-print OUT $json->encode ($attractorTable);
+print OUT $json->encode ($output);
 close (OUT) or die ("<br>ERROR: Cannot close the file for output. <br>");
 
 exit;
@@ -170,26 +116,160 @@ exit;
 
 =pod
 
-error_checking ();
+errorCheck ();
 
-Checks if the user enters the options/parameters correctly
+Checks if the user enters the options/parameters correctly and there is any internal error
 
 =cut
 
-sub error_checking {
+sub errorCheck {
   
-  # num_functions and num_variables
-  unless ($num_functions == $num_variables) {
-    print ("<br>INTERNAL ERROR: The number of variables, $num_variables, must be equal to the number of update rules, $num_functions. <br>");
+  # num_rules and num_variables
+  unless (($input->{"numberVariables"} == $num_variables) || ($num_rules == $num_variables)) {
+    print ("<br>INTERNAL ERROR: There is inconsistency betwen the number of variables ($num_variables) and numberVariables (", $input->{"numberVariables"}, ") OR the number of update rules ($num_rules). <br>");
     exit;
   }
 
   # samplingSize
-  if (isnot_number ($samplingSize) || ($samplingSize <= 0)) {
-    print ("<br>ERROR: The sampling size must be a positive number. <br>");
+  if (isnot_number ($samplingSize) || ($samplingSize < 1) || ($samplingSize > $max_num_sampling)) {
+    print ("<br>ERROR: The sampling size must be a number between 1 and $max_num_sampling. <br>");
     exit;
   }
 
+}
+
+############################################################
+
+=pod
+
+get_updateRules ();
+
+Returns 2 hash tables one of which is for polynomial functions and another is for transition tables
+
+=cut
+
+sub get_updateRules {
+  my $polyFuncs = {};
+  my $tranTables = {};
+
+  foreach my $i (@$updateRules) {
+    my $key = $i->{"target"};
+    $polyFuncs->{$key} = $i->{"functions"}->[0]->{"polynomialFunction"};
+
+    my $value_tt = {};
+    foreach my $j (@{$i->{"functions"}->[0]->{"transitionTable"}}) {
+      my $k = join (' ', $j->[0]);
+      $value_tt->{$k} = $j->[1];
+    }
+    
+    $tranTables->{$key} = $value_tt;
+  }
+  
+  return ($polyFuncs, $tranTables);
+}
+
+############################################################
+
+=pod
+
+get_basinOfAttractors ();
+
+Returns 2 hash tables one of which is for initialState-attractor and another is for attractor-size
+
+=cut
+
+sub get_basinOfAttractors {
+  my $is_attractor_table = {};
+  my $attractor_size_table = {};
+  
+  my $stateSpaceSize = $num_states ** $num_variables;
+  my $length = 5;
+  my %state_attractor_table;
+  my %attractor_table;
+  
+  for (my $index = 1; $index <= $samplingSize; $index++) {
+    my $n = int (rand ($stateSpaceSize));
+    my $is = convert_from_integer_to_state ($n);
+    
+    if (exists $is_attractor_table->{$is}) {
+      next;
+    }
+    
+    my @array = ($is);
+    
+  attr: while (1) {
+      for (my $n = 1; $n <= $length; $n++) {
+	push (@array, get_nextstate ($array[-1]));
+      }
+      
+      my $arraysize = scalar @array;
+      
+      for (my $j = 0; $j < $arraysize - 1; $j++) {
+	for (my $k = $j + 1; $k < $arraysize; $k++) {
+	  if ($array[$j] eq $array[$k]) {
+	    
+	    my @sub_array = @array[$j ... $k - 1];
+	    my $sortedAttractor = join (' , ', sort {$a cmp $b} @sub_array);
+	    
+	    unless (exists $attractor_size_table->{$sortedAttractor} ) {
+	      my $attractor = join (' -> ', @sub_array);
+	      $attractor_size_table->{$sortedAttractor} = [$attractor, 0];
+	    }
+	    
+	    for (my $s = 0; $s < $k; $s++) {
+	      my $a = $array[$s];
+	      unless (exists $is_attractor_table->{$a}) {
+		$is_attractor_table->{$a} = $sortedAttractor;
+		${$attractor_size_table->{$sortedAttractor}}[1]++;
+	      }
+	    }
+	    last attr;
+	  }
+	}
+      }
+      
+      push (@array, get_nextstate ($array[-1]));
+      
+    } # end of while loop
+    
+  } # end of for loop
+  
+  return ($is_attractor_table, $attractor_size_table);
+}
+
+############################################################
+
+=pod
+
+format_output ();
+
+Returns a hash table containing proper output for this code.
+
+=cut
+
+sub format_output {
+  my @attractorsAndSizes = ();
+
+  foreach my $attrAndSize (values %$basinOfAttractors) {
+    my %hash = (
+		"attractor" => $attrAndSize->[0],
+		"size" => $attrAndSize->[1]
+	       );
+    push (@attractorsAndSizes, \%hash);
+    
+  }
+
+  my $output = {"output" => [{
+			      "type" => "randomSamplingOutput",
+			      "description" => "basin of attractors of a large system using random sampling",
+			      "fieldCardinality" => $num_states,
+			      "numberVariables" => $num_variables,
+			      "samplingSize" => $samplingSize,
+			      "basinOfAttractors" => @attractorsAndSizes
+			     }]
+	       };
+  
+  return $output;
 }
 
 ############################################################
@@ -215,51 +295,19 @@ sub isnot_number {
 
 ############################################################
 
-# Returns the next state (as an integer) of a given state (as an integer)
+=pod
 
-sub get_nextstate {
-  my $state = shift;
-  my @currentState = convert_from_integer_to_state ($state);
-  my @nextState;
-  
-  for (my $i = 1; $i <= $num_variables; $i++) {
-    my $func = $updateFunctions->{"x$i"}->{"polynomialFunction"};
+convert_from_integer_to_state ($n);
 
-    for (my $j = 1; $j <= $num_variables; $j++) {
-      $func =~ s/x[$j]/\($currentState[$j - 1]\)/g;
-    }
-    
-    $nextState[$i - 1] = eval ($func) % $num_states;
-   }
+Converts the integer representation of a state to state itself as a string
 
-  my $nextState = convert_from_state_to_integer (\@nextState);
-  return $nextState;
-}
-
-################################################################################
-
-# Converts a given state (as a ref of array) to its integer representation and adds 1 for convenience
-
-sub convert_from_state_to_integer {
-  my $state = shift;
-  my $int_rep = 1;
-
-  for (my $i = 0; $i < $num_variables; $i++) {
-    $int_rep += $state->[$num_variables - $i - 1] * ($num_states ** $i);
-  }
-  return $int_rep;
-}
-
-################################################################################
-
-# Converts the integer representation of a state to state itself (as an array)
+=cut
 
 sub convert_from_integer_to_state {
   my $n = shift;
 
   my ($quotient, $remainder);
   my @state = ();
-  $n--;
 
   do {
     $quotient = int $n / $num_states;
@@ -277,30 +325,44 @@ sub convert_from_integer_to_state {
   }
 
   @state = reverse @state;
-  return @state;
+  my $str_state = join (" ", @state);
+  return $str_state;
 }
 
-################################################################################
+############################################################
 
-# Format the attractors
+# Returns the next state of a given state as a string
 
-sub format_attractors {
-  my $table = shift;
-  my %formattedTable;
+sub get_nextstate {
+  my $state = shift;
+  my @currState = split (" ", $state);
 
-  foreach my $value (values %$table) {
-    my @array1 = split (' -> ', $value->[0]);
-    my @array2 = ();
+  my @nextState;
+  
+  for (my $i = 1; $i <= @currState; $i++) {
+    my $func = $polyFuncs->{"x$i"};
+    my $tt = $tranTables->{"x$i"};
 
-    for (my $i = 0; $i < scalar @array1; $i++) {
-      $array2[$i] = join (' ', convert_from_integer_to_state ($array1[$i]));
+    if ($func) {
+      for (my $j = 1; $j <= @currState; $j++) {
+  	$func =~ s/x[$j]/\($currState[$j - 1]\)/g;
+      }
+      
+      $nextState[$i - 1] = eval ($func) % $num_states;
     }
-    
-    my $attractor = join (' -> ', @array2);
-    $formattedTable{$attractor} = $value->[1];
-  }
+    elsif ($tt) {
 
-  return \%formattedTable;
+      # TO_DO: use the transition table information if available
+
+    }
+    else {
+      print ("<br>INTERNAL ERROR: Neither polynomial function or transition table is available as an update rule for x$i. <br>");
+      exit;
+    }
+  }
+  
+  my $str_nextState = join (" ", @nextState);
+  return $str_nextState;
 }
 
-################################################################################
+############################################################
